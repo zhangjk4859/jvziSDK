c36c8e3907a9b418e62e0bdece43d54ab72a3133
zhangjk4859 <573581325@qq.com>
2016-07-05 10:05:48 +0800

    新建了静态库工程

---
+++ juziSDK/juziSDK/juziSDK.h
@@ -0,0 +1,13 @@
+//
+//  juziSDK.h
+//  juziSDK
+//
+//  Created by 张俊凯 on 16/7/5.
+//  Copyright © 2016年 禾浦信息科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface juziSDK : NSObject
+
+@end
---
+++ juziSDK/juziSDK/juziSDK.m
@@ -0,0 +1,13 @@
+//
+//  juziSDK.m
+//  juziSDK
+//
+//  Created by 张俊凯 on 16/7/5.
+//  Copyright © 2016年 禾浦信息科技. All rights reserved.
+//
+
+#import "juziSDK.h"
+
+@implementation juziSDK
+
+@end
